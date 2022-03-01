5f358808fd3dd1a238473580e624001fd5a40a26
Kevin Gomes <kevinmg50@gmail.com>
2022-03-01 18:01:16 -0300

    feat(controller): inserção de imagem via api

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,10 +1,24 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Moment from 'App/Models/Moment'
+import Application from '@ioc:Adonis/Core/Application'
+import { v4 as uuidv4 } from 'uuid'
 export default class MomentsController {
+  private validationOptions = {
+    type: ['image'],
+    size: '5mb',
+  }
   public async store({ request, response }: HttpContextContract) {
     const body = request.body()
-    const moment = await Moment.create(body)
+    const image = request.file('image', this.validationOptions)
+    if (image) {
+      const imageName = `${uuidv4()}.${image.extname}`
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName,
+      })
+      body.image = imageName
+    }
 
+    const moment = await Moment.create(body)
     response.status(201)
     return {
       message: 'Momento criado com sucesso!',
